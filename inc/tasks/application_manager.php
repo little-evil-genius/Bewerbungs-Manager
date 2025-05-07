33287a93547b41dcc40b01f892b59f7309262bb6
Lara <75126217+little-evil-genius@users.noreply.github.com>
2025-05-07 17:01:12 +0200

    Add files via upload

---
+++ inc/tasks/application_manager.php
@@ -0,0 +1,51 @@
+<?php
+
+function task_application_manager($task){
+
+    global $db, $mybb, $lang, $cache;
+
+    $applicationgroup = $mybb->settings['application_manager_applicationgroup'];
+    $control_period = $mybb->settings['application_manager_control_period'];
+
+    // Neue Bewerber hinzufügen
+    $newApplicants = $db->query("SELECT uid, regdate FROM ".TABLE_PREFIX."users
+    WHERE usergroup = $applicationgroup
+    AND uid NOT IN(SELECT uid FROM ".TABLE_PREFIX."applicants
+    )");
+
+    while ($new = $db->fetch_array($newApplicants)) {
+
+        $regDate = new DateTime($new['regdate']);
+        $regDate->setTime(0, 0, 0);
+        $regDate->modify("+{$control_period} days");
+        $application_deadline = $db->escape_string($regDate->format("Y-m-d"));
+
+        $insertApplicant = array(
+            'uid' => $new['uid'],
+            'application_deadline' => $application_deadline,
+        );
+
+        $db->insert_query('applicants', $insertApplicant);
+    }
+
+    // gelöschte Accounts entfernen
+    $deletedApplicants = $db->query("SELECT uid FROM ".TABLE_PREFIX."application_manager
+    WHERE uid NOT IN(SELECT uid FROM ".TABLE_PREFIX."users)
+    ");
+
+    while ($deleted = $db->fetch_array($deletedApplicants)) {
+        $db->delete_query('application_manager', 'uid = '.$deleted['uid']);
+    }
+
+    // angenommen Accounts entfernen
+    $oldApplicants = $db->query("SELECT uid FROM ".TABLE_PREFIX."users
+    WHERE usergroup != $applicationgroup
+    AND uid IN(SELECT uid FROM ".TABLE_PREFIX."applicants)
+    ");
+
+    while ($old = $db->fetch_array($oldApplicants)) {
+        $db->delete_query('applicants', 'uid = '.$old['uid']);
+    }
+
+    add_task_log($task, 'Gelöschte und angenommene Bewerber:innen wurde entfernt.');
+}
